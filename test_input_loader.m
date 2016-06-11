46a1d1b8f162f811dc9d2ae2d07018c5c4b95941
ifenglin <bee2016@gmail.com>
2016-06-11 21:08:47 +0200

    Add files via upload

---
+++ test_input_loader.m
@@ -0,0 +1,7 @@
+% an example to call input_loader.m
+% create a 64 x 64 x 3 x 5 complex matrix and save it
+a = zeros(64,64,3,5);
+data = complex(a, 1);
+save 'testdata.mat' data;
+% load the matrix
+input_loader('testdata.mat');
